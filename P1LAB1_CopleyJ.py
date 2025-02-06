89fb86a15d2025196805538350d2e9edec0340a3
copleyj2640 <copleyj2640@student.faytechcc.edu>
2025-02-06 14:20:53 -0500

    Add files via upload

---
+++ P1LAB1_CopleyJ.py
@@ -0,0 +1,10 @@
+#John Copley
+#02-6-2025
+#P1LAB1
+#This program will demo python code
+ 
+fname = input ('Enter your first name: ')
+
+lname = input ('Enter your last name: ')
+
+print("Hello, "+ str(fname)+" "+str(lname)+"! Welcome to CTI-110")
